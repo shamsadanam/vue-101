bbc5532dd5fbea6e2f60e4a8fb3e758152600f73
shamsad.anam <shamsad.anam@gmail.com>
2023-01-02 09:47:12 +0600

    fc

---
+++ src/components/Counter.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>Counter</h1>
+  <h4>{{ count }}</h4>
+  <button @click="count--">-</button>
+  <button @click="count++">+</button>
+</template>
---
+++ src/views/CounterView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Counter from "../components/Counter.vue";
+</script>
+
+<template>
+  <div class="about">
+    <div>
+      <h1>This is the Counter page</h1>
+      <Counter />
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
